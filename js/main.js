2f5a4f0f3fad7772e5a6ede8cc44b8eef81cd74a
deli-off <dzamsedovnazif1@gmail.com>
2022-11-13 16:40:34 +0500

    start homework

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ let car = {
 
 let cars = [
     {
+        id: Math.random(),
         mark: "bmw",
         color: "white",
         engine: "1.5",
@@ -21,6 +22,7 @@ let cars = [
         img: 'https://kolesa-uploads.ru/-/24165599-a9d1-4e6d-aeab-6b873972da6c/bmw-i8-1.jpg'
     },
     {
+        id: Math.random(),
         mark: "camry",
         color: "black",
         engine: "V6",
@@ -31,6 +33,7 @@ let cars = [
         img: 'https://a.d-cd.net/f05f5bes-960.jpg'
     },
     {
+        id: Math.random(),
         mark: "tesla",
         color: "white",
         engine: "electro",
@@ -41,6 +44,7 @@ let cars = [
         img: 'https://cdna.artstation.com/p/assets/covers/images/014/856/578/large/alberto-luque-c-13-8k-pequena.jpg?1545931509'
     },
     {
+        id: Math.random(),
         mark: "lamborgini",
         color: "orange",
         engine: "v12",
@@ -51,6 +55,7 @@ let cars = [
         img: "https://i.playground.ru/i/pix/2543123/image.jpg"
     },
     {
+        id: Math.random(),
         mark: "bmw",
         color: "blue",
         engine: "V10-cylinder engine",
@@ -61,6 +66,7 @@ let cars = [
         img: "https://1gai.ru/uploads/posts/2013-12/1386967351_new-bmw-m3-sedan-192.jpg"
     },
     {
+        id: Math.random(),
         mark: "lotus",
         color: "red",
         engine: "2ZZ-GE",
@@ -71,6 +77,7 @@ let cars = [
         img: "https://wroom.ru/i2/carpic/10044.jpg"
     },
     {
+        id: Math.random(),
         mark: "malibu 2",
         color: "black",
         engine: "Facelift 1.5 LT",
@@ -81,6 +88,7 @@ let cars = [
         img: "https://car24.uz/wp-content/uploads/2019/11/photo5238192627768210177.jpg"
     },
     {
+        id: Math.random(),
         mark: "toyota",
         color: "white",
         engine: "v8",
@@ -91,6 +99,7 @@ let cars = [
         img: "https://motor.ru/thumb/908x0/filters:quality(75):no_upscale()/imgs/2021/01/29/09/4483637/d7be1e5c6dc4ab68c0a16b4e15ffd31364b68e7f.jpg"
     },
     {
+        id: Math.random(),
         mark: "spark",
         color: "green",
         engine: "M300",
@@ -101,6 +110,7 @@ let cars = [
         img: "https://quto.ru/service-imgs/4c/03/ca/a7/4c03caa704002.jpeg"
     },
     {
+        id: Math.random(),
         mark: "toyota",
         color: "white",
         engine: "v8",
@@ -111,6 +121,7 @@ let cars = [
         img: "https://motor.ru/thumb/908x0/filters:quality(75):no_upscale()/imgs/2021/01/29/09/4483637/d7be1e5c6dc4ab68c0a16b4e15ffd31364b68e7f.jpg"
     },
     {
+        id: Math.random(),
         mark: "tico",
         color: "black",
         engine: "suzuki alto",
@@ -121,6 +132,7 @@ let cars = [
         img: "https://s.auto.drom.ru/i24198/c/photos/generations/500x_daewoo_tico_g5931.jpg?559589"
     },
     {
+        id: Math.random(),
         mark: "matiz",
         color: "yellow",
         engine: "daewoo f8CV",
@@ -131,6 +143,7 @@ let cars = [
         img: "https://autoreview.ru/images/Article/1649/Article_164980_860_575.jpg"
     },
     {
+        id: Math.random(),
         mark: "Mercedes",
         color: "sky-blue",
         engine: "v10",
@@ -141,6 +154,7 @@ let cars = [
         img: "https://s.auto.drom.ru/i24219/c/photos/generations/500x_mercedes-benz_amg_gt_g8242.jpg?796841"
     },
     {
+        id: Math.random(),
         mark: "Audi",
         color: "grey",
         engine: "v8",
@@ -151,6 +165,7 @@ let cars = [
         img: "https://s.auto.drom.ru/img4/catalog/photos/generations/500x_audi_r8_g1265.jpg?109709"
     },
     {
+        id: Math.random(),
         mark: "Nissan",
         color: "black",
         engine: "v100",
@@ -161,6 +176,7 @@ let cars = [
         img: "https://s.auto.drom.ru/i24196/c/photos/generations/500x_nissan_gt-r_g4800.jpg?495634"
     },
     {
+        id: Math.random(),
         mark: "Kia k5",
         color: "blue",
         engine: "2.6",
@@ -171,6 +187,7 @@ let cars = [
         img: "https://www.autostrada.uz/wp-content/uploads/2020/09/5eddffb2ec05c4da4800002c.jpg"
     },
     {
+        id: Math.random(),
         mark: "camaro",
         color: "white",
         engine: "v6",
@@ -181,6 +198,7 @@ let cars = [
         img: "  https://autoartmodels.de/wp-content/uploads/2020/04/71207a-scaled.jpg"
     },
     {
+        id: Math.random(),
         mark: "Tesla Plaid",
         color: "white",
         engine: "electric car",
@@ -192,6 +210,7 @@ let cars = [
     },
     {
 
+        id: Math.random(),
         mark: 'malibu-2-premier',
         color: "white",
         engine: "2.0",
@@ -202,6 +221,7 @@ let cars = [
         img: 'https://apollo-olx.cdnvideo.ru/v1/files/me5ci48ymrec-UZ/image;s=2000x1335'
     },
     {
+        id: Math.random(),
         mark: 'Mersedes-gelik',
         color: 'black',
         engine: 'v12',
@@ -212,6 +232,7 @@ let cars = [
         img: 'https://krot.info/uploads/posts/2021-10/1634347714_58-krot-mobi-p-mersedes-gelik-mashini-krasivo-foto-58.jpg'
     },
     {
+        id: Math.random(),
         mark: 'kopeyka',
         color: 'black',
         engine: 1.2,
@@ -271,7 +292,7 @@ let cars = [
 //     }
 // }
 
-let max = cars.reduce((a, b) => a.price < b.price ? a : b);
+let max = cars.reduce((a, b) => a.price > b.price ? a : b);
 
 
 cars.forEach(item => {
@@ -279,6 +300,8 @@ cars.forEach(item => {
 
     document.write(`
         <div class = "block">
+            <h3>car id: ${item.id}</h3>
+
             Mark: <b>${item.mark}</b>  <i style = "color: ${item.color};">${item.color}</i>  <br>
             Hashatch: <b>${item.hasHatch}</b> <br>
             Is box: <b>${item.isBox}</b> <br>
@@ -290,3 +313,7 @@ cars.forEach(item => {
         </div>
     `)
 })
+
+setTimeout(() => {
+    prompt('which car')
+}, 3000);
